0c98dbb710e57edfc1ee5a155e83ca822cfd9a33
KZAR <kryh2426@gmail.com>
2026-04-12 22:34:56 +0200

    const SLIDE_DURATION = 6000

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ reveals.forEach(el => observer.observe(el));
 
     if (!heroSlides.length || !progressBar) return;
 
-    const SLIDE_DURATION = 5000;
+    const SLIDE_DURATION = 6000;
     let current = 0;
     let timer = null;
 
